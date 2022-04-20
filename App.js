9fbe7784afa88c0f3899a68129865b3a9a358770
alanlion1 <104042595+alanlion1@users.noreply.github.com>
2022-04-19 20:22:23 -0500

    react.native.course

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola Loudwin, como estas, yo aqui muy bien</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
