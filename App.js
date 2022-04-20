8227ac7590b6fba178916c58174578efab5acad0
Loudwin <54119098+Loudwin@users.noreply.github.com>
2022-04-19 20:47:30 -0500

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Que onda como estás</Text>
+      <Text> estás</Txt>a
       <StatusBar style="auto" />
     </View>
   );
